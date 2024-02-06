104cac64826f50a85ec8882140d81bd98cfe5710
uYesu Crestu <116444200+Vambane@users.noreply.github.com>
2024-02-07 00:18:34 +0200

    added a selectbox, fixed search

--- streamlit/home.py
+++ streamlit/home.py
@@ -59,7 +59,8 @@ def get_player_recommendations(player_name, age_filter=None, num_recommendation=
 st.title('Player Recommendation App')
 
 # Sidebar with user input
-player_name = st.sidebar.text_input('Enter player name:')
+column_name = 'Name'
+player_name = st.selectbox('Enter player name:', options=df[column_name].unique())
 age_filter = st.sidebar.slider('Age Filter', 15, 45, 30)
 num_recommendations = st.sidebar.slider('Number of recommendations', 1, 20, 10)
 
